13a689ff7da4f1cad592ffd3e56faf9d5955b5ea
Jon Waters <waters1228@gmail.com>
2021-01-02 15:29:22 -0500

    Add class Country

---
+++ src/Model/Country.java
@@ -0,0 +1,74 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class Country {
+
+    private int countryID;
+
+    private String countryName;
+
+    private LocalDateTime createDate;
+
+    private String createdBy;
+
+    private LocalDateTime lastUpdateTime;
+
+    private String lastUpdatedBy;
+
+    public Country(String countryName, LocalDateTime createDate, String createdBy, LocalDateTime lastUpdateTime, String lastUpdatedBy) {
+        this.countryName = countryName;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdateTime = lastUpdateTime;
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public int getCountryID() {
+        return countryID;
+    }
+
+    public void setCountryID(int countryID) {
+        this.countryID = countryID;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(LocalDateTime lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+}
