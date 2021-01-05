e771f91fa9c09eec04d3b3f6a9b8852cf74219c8
Jon Waters <waters1228@gmail.com>
2021-01-04 19:34:04 -0500

    Add class User

---
+++ src/Model/User.java
@@ -0,0 +1,86 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private int userID;
+
+    private String userName;
+
+    private String password;
+
+    private LocalDateTime createDate;
+
+    private String createdBy;
+
+    private LocalDateTime lastUpdateTime;
+
+    private String lastUpdatedBy;
+
+    public User(String userName, String password, LocalDateTime createDate,
+                String createdBy, LocalDateTime lastUpdateTime, String lastUpdatedBy) {
+        this.userName = userName;
+        this.password = password;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdateTime = lastUpdateTime;
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(LocalDateTime lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+}
