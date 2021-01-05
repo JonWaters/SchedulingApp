173d21846f33ee2405f3b9976c27594385911086
Jon Waters <waters1228@gmail.com>
2021-01-04 19:01:02 -0500

    Add class Division

---
+++ src/Model/Division.java
@@ -0,0 +1,83 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class Division {
+
+    private int divisionID;
+
+    private String divisionName;
+
+    private LocalDateTime createDate;
+
+    private String createdBy;
+
+    private LocalDateTime lastUpdateTime;
+
+    private String lastUpdatedBy;
+
+    private int countryID;
+
+    public Division(String divisionName, String createdBy, String lastUpdatedBy, int countryID) {
+        this.divisionName = divisionName;
+        this.createdBy = createdBy;
+        this.lastUpdatedBy = lastUpdatedBy;
+        this.countryID = countryID;
+    }
+
+    public int getDivisionID() {
+        return divisionID;
+    }
+
+    public void setDivisionID(int divisionID) {
+        this.divisionID = divisionID;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
+
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(LocalDateTime lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public int getCountryID() {
+        return countryID;
+    }
+
+    public void setCountryID(int countryID) {
+        this.countryID = countryID;
+    }
+}
