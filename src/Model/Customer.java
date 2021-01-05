1376d90c57c74ca9ce6076ef44ea4e1d96ba2a4d
Jon Waters <waters1228@gmail.com>
2021-01-04 19:18:43 -0500

    Add class Customer

---
+++ src/Model/Customer.java
@@ -0,0 +1,120 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class Customer {
+
+    private int customerID;
+
+    private String customerName;
+
+    private String address;
+
+    private String postalCode;
+
+    private String phone;
+
+    private LocalDateTime createDate;
+
+    private String createdBy;
+
+    private LocalDateTime lastUpdateTime;
+
+    private String lastUpdatedBy;
+
+    private int divisionID;
+
+    public Customer(String customerName, String address, String postalCode,
+                    String phone, LocalDateTime createDate, String createdBy,
+                    LocalDateTime lastUpdateTime, String lastUpdatedBy, int divisionID) {
+        this.customerName = customerName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.phone = phone;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdateTime = lastUpdateTime;
+        this.lastUpdatedBy = lastUpdatedBy;
+        this.divisionID = divisionID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(LocalDateTime lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public int getDivisionID() {
+        return divisionID;
+    }
+
+    public void setDivisionID(int divisionID) {
+        this.divisionID = divisionID;
+    }
+}
