7a8a25654d08d4b1713f3c8675fd3dc280ee87a8
Jon Waters <waters1228@gmail.com>
2021-01-05 19:14:15 -0500

    Update class DbQuery to use PreparedStatement

--- src/utils/DbQuery.java
+++ src/utils/DbQuery.java
@@ -1,19 +1,19 @@
 package utils;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DbQuery {
 
-    private static Statement statement;
+    private static PreparedStatement statement;
 
     public static void setPreparedStatement(Connection conn, String sqlStatement) throws SQLException {
 
         statement = conn.prepareStatement(sqlStatement);
     }
 
-    public static Statement getPreparedStatement() {
+    public static PreparedStatement getPreparedStatement() {
 
         return statement;
     }
