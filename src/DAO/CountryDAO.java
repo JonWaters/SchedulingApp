ff04d74e865a09cba4b49112066d7318492e065a
Jon Waters <waters1228@gmail.com>
2021-01-06 11:59:33 -0500

    Update insert() for CountryDAO

--- src/DAO/CountryDAO.java
+++ src/DAO/CountryDAO.java
@@ -4,28 +4,35 @@ import Model.Country;
 import sample.Main;
 import utils.DbQuery;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 
 public class CountryDAO {
 
     private final static Connection conn = Main.conn;
 
-    public static Country insert(Country country) throws SQLException {
+    public static void insert(Country country) throws SQLException {
 
-        String sqlStatement = "INSERT INTO Countries(Country, Create_Date, Created_By, Last_Update, " +
-                "Last_Updated_by) VALUES(?, ?, ?, ?, ?)";
+        try {
+            String sqlStatement = "INSERT INTO Countries(Country, Create_Date, Created_By, Last_Update, " +
+                    "Last_Updated_by) VALUES(?, NOW(), ?, NOW(), ?)";
 
-        DbQuery.setPreparedStatement(conn, sqlStatement);
+            DbQuery.setPreparedStatement(conn, sqlStatement);
 
-        PreparedStatement ps = DbQuery.getPreparedStatement();
+            PreparedStatement ps = DbQuery.getPreparedStatement();
 
-        ps.setString(1, country.getCountryName());
-        ps.setTimestamp(2, Timestamp.valueOf(country.getCreateDate()));
+            ps.setString(1, country.getCountryName());
+            ps.setString(2, country.getCreatedBy());
+            ps.setString(3, country.getLastUpdatedBy());
 
-        return country;
+            ps.execute();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            country.setCountryID(rs.getInt(1));
+        }
+        catch(SQLException e){
+            e.getMessage()
+        }
     }
 }
 
