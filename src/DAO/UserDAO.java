e2dbf9c5b5b35475f58981637af8236bcbf8fadb
Jon Waters <waters1228@gmail.com>
2021-01-08 19:35:29 -0500

    Add create() to class UserDAO

--- src/DAO/UserDAO.java
+++ src/DAO/UserDAO.java
@@ -1,11 +1,57 @@
 package DAO;
 
+import Model.User;
 import sample.Main;
+import utils.DbQuery;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAO {
 
     private final static Connection conn = Main.conn;
-    
+
+    public static void create(User user) throws SQLException {
+
+        try {
+            String sqlStatement = "INSERT INTO users(User_Name, Password, Create_Date, " +
+                    "Created_By, Last_Update, Last_Updated_By) VALUES(?, ? NOW(), ?, NOW(), ?)";
+
+            DbQuery.setPreparedStatement(conn, sqlStatement);
+
+            PreparedStatement ps = DbQuery.getPreparedStatement();
+
+            ps.setString(1, user.getUserName());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getCreatedBy());
+            ps.setString(4, user.getLastUpdatedBy());
+
+            ps.execute();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            user.setUserID(rs.getInt(1));
+
+            sqlStatement = "SELECT * FROM users WHERE User_ID = ?";
+
+            DbQuery.setPreparedStatement(conn, sqlStatement);
+
+            ps = DbQuery.getPreparedStatement();
+
+            ps.setInt(1, user.getUserID());
+
+            ps.execute();
+
+            rs = ps.getResultSet();
+            rs.next();
+
+            user.setCreateDate(rs.getTimestamp("Create_Date").toLocalDateTime());
+            user.setLastUpdateTime(rs.getTimestamp("Last_Update").toLocalDateTime());
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
