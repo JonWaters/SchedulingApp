37bdb75f81bdca2f63907da6fe8ac1d403110ba1
Jon Waters <waters1228@gmail.com>
2021-01-19 20:04:21 -0500

    Add time spinners to NewAppointmentController

--- src/Controller/NewAppointmentController.java
+++ src/Controller/NewAppointmentController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.Contact;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -7,17 +8,19 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.util.converter.LocalTimeStringConverter;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class NewAppointmentController implements Initializable {
+
+    private final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
 
     @FXML
     private TextField titleText;
@@ -38,7 +41,7 @@ public class NewAppointmentController implements Initializable {
     private DatePicker endDatePicker;
 
     @FXML
-    private ComboBox<?> contactComboBox;
+    private ComboBox<Contact> contactComboBox;
 
     @FXML
     private ComboBox<?> customerComboBox;
@@ -47,10 +50,10 @@ public class NewAppointmentController implements Initializable {
     private ComboBox<?> userComboBox;
 
     @FXML
-    private Spinner<?> startTimeSpinner;
+    private Spinner<LocalTime> startTimeSpinner;
 
     @FXML
-    private Spinner<?> endTimeSpinner;
+    private Spinner<LocalTime> endTimeSpinner;
 
     @FXML
     void cancelButtonAction(ActionEvent event) throws IOException {
@@ -83,5 +86,42 @@ public class NewAppointmentController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        startSVF.setValue(LocalTime.of(8, 00));
+        startTimeSpinner.setValueFactory(startSVF);
+        endSVF.setValue(LocalTime.of(17, 00));
+        endTimeSpinner.setValueFactory(endSVF);
+        
     }
+
+    SpinnerValueFactory startSVF = new SpinnerValueFactory<LocalTime>() {
+        {
+            setConverter(new LocalTimeStringConverter(timeFormat,null));
+        }
+        @Override public void decrement(int steps) {
+            LocalTime localTime = (LocalTime) getValue();
+            setValue(localTime.plusHours(steps));
+            setValue(localTime.plusMinutes(steps + 14));
+        }
+        @Override public void increment(int steps) {
+            LocalTime localTime = (LocalTime) getValue();
+            setValue(localTime.minusHours(steps));
+            setValue(localTime.minusMinutes(16 - steps));
+        }
+    };
+
+    SpinnerValueFactory endSVF = new SpinnerValueFactory<LocalTime>() {
+        {
+            setConverter(new LocalTimeStringConverter(timeFormat,null));
+        }
+        @Override public void decrement(int steps) {
+            LocalTime localTime = (LocalTime) getValue();
+            setValue(localTime.plusHours(steps));
+            setValue(localTime.plusMinutes(steps + 14));
+        }
+        @Override public void increment(int steps) {
+            LocalTime localTime = (LocalTime) getValue();
+            setValue(localTime.minusHours(steps));
+            setValue(localTime.minusMinutes(16 - steps));
+        }
+    };
 }
