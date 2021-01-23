a7ab49b81f049fa96166a6ae6ab419d60a8e26b7
Jon Waters <waters1228@gmail.com>
2021-01-23 11:59:39 -0500

    Add displayAllCustomers() toCustomersController

--- src/Controller/CustomersController.java
+++ src/Controller/CustomersController.java
@@ -1,5 +1,13 @@
 package Controller;
 
+import DAO.AppointmentDAO;
+import DAO.CustomerDAO;
+import Model.Appointment;
+import Model.AppointmentDisplay;
+import Model.Customer;
+import Model.CustomerDisplay;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,31 +17,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class CustomersController implements Initializable {
 
     @FXML
-    private TableView<?> customersTable;
+    private TableView<Customer> customersTable;
 
     @FXML
-    private TableColumn<?, ?> nameColumn;
+    private TableColumn<Customer, String> nameColumn;
 
     @FXML
-    private TableColumn<?, ?> addressColumn;
+    private TableColumn<Customer, String> addressColumn;
 
     @FXML
-    private TableColumn<?, ?> postalColumn;
+    private TableColumn<Customer, String> postalColumn;
 
     @FXML
-    private TableColumn<?, ?> phoneColumn;
+    private TableColumn<Customer, String> phoneColumn;
 
     @FXML
-    private TableColumn<?, ?> divisionColumn;
+    private TableColumn<Customer, String> divisionColumn;
 
     @FXML
     void backButtonAction(ActionEvent event) throws IOException {
@@ -80,6 +90,38 @@ public class CustomersController implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("customerName"));
+        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+        postalColumn.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        divisionColumn.setCellValueFactory(new PropertyValueFactory<>("divisionName"));
+
+        try {
+            displayAllCustomers();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void displayAllCustomers() throws SQLException {
+
+        try {
+            ObservableList<Customer> dbCustomers = CustomerDAO.selectAll();
+            ObservableList<Customer> customers = FXCollections.observableArrayList();
+
+            for (Customer customer : dbCustomers) {
+
+                CustomerDisplay newCustomer = new CustomerDisplay(customer);
+                customers.add(newCustomer);
+            }
+
+            customersTable.setItems(customers);
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
