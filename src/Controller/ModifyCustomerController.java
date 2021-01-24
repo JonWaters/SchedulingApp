b6fad5f3b51c65ad34520e1d2cff62bba44735b7
Jon Waters <waters1228@gmail.com>
2021-01-24 15:03:35 -0500

    Duplicate file for ModifyCustomerController

--- src/Controller/ModifyCustomerController.java
+++ src/Controller/ModifyCustomerController.java
@@ -1,5 +1,15 @@
 package Controller;
 
+import DAO.CountryDAO;
+import DAO.CustomerDAO;
+import DAO.DivisionDAO;
+import Model.Country;
+import Model.Customer;
+import Model.Division;
+import Model.User;
+import Utils.VerifyCust;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -7,18 +17,23 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ModifyCustomerController implements Initializable {
 
-    @FXML
-    private TextField customerIdText;
+    private ObservableList<Country> countryList = FXCollections.observableArrayList();
+
+    private User currentUser = LoginController.getCurrentUser();
 
     @FXML
     private TextField nameText;
@@ -33,34 +48,88 @@ public class ModifyCustomerController implements Initializable {
     private TextField phoneText;
 
     @FXML
-    private ComboBox<?> countryComboBox;
+    private ComboBox<Country> countryComboBox;
 
     @FXML
-    private ComboBox<?> divisionComboBox;
+    private ComboBox<Division> divisionComboBox;
 
     @FXML
     void cancelButtonAction(ActionEvent event) throws IOException {
 
-        Parent parent = FXMLLoader.load(getClass().getResource("../View/Customers.fxml"));
-        Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Alert");
+        alert.setContentText("Do you want cancel changes and return to the Customers screen?");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+
+            Parent parent = FXMLLoader.load(getClass().getResource("../View/Customers.fxml"));
+            Scene scene = new Scene(parent);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     @FXML
     void countryComboBoxAction(ActionEvent event) {
 
+        try {
+            Country selectedCountry = countryComboBox.getSelectionModel().getSelectedItem();
+            ObservableList<Division> dbDivisions = DivisionDAO.selectAll();
+            ObservableList<Division> divisionByCountry = FXCollections.observableArrayList();
+
+            for (Division division : dbDivisions) {
+
+                if (division.getCountryID() == selectedCountry.getCountryID()) {
+                    divisionByCountry.add(division);
+                }
+            }
+
+            divisionComboBox.setItems(divisionByCountry);
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @FXML
     void saveButtonAction(ActionEvent event) throws IOException {
 
-        Parent parent = FXMLLoader.load(getClass().getResource("../View/Customers.fxml"));
-        Scene scene = new Scene(parent);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+        try {
+            String customerName = nameText.getText();
+            String address = addressText.getText();
+            String postalCode = postalText.getText();
+            String phone = phoneText.getText();
+            String createdBy = currentUser.getUserName();
+            String lastUpdatedBy = currentUser.getUserName();
+            int divisionID = divisionComboBox.getSelectionModel().getSelectedItem().getDivisionID();
+
+            Customer newCustomer = new Customer(customerName, address, postalCode, phone, createdBy,
+                    lastUpdatedBy, divisionID);
+
+            if (!VerifyCust.fieldsEmpty(newCustomer)) {
+
+                CustomerDAO.create(newCustomer);
+
+                Parent parent = FXMLLoader.load(getClass().getResource("../View/Customers.fxml"));
+                Scene scene = new Scene(parent);
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            }
+        }
+        catch(NullPointerException e) {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+
+            alert.setTitle("Error");
+            alert.setHeaderText("One or more fields are not selected.");
+            alert.showAndWait();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     /**
@@ -74,5 +143,17 @@ public class ModifyCustomerController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        setDefaultValues();
+    }
+
+    private void setDefaultValues() {
+
+        try {
+            countryList = CountryDAO.selectAll();
+            countryComboBox.setItems(countryList);
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
