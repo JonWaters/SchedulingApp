a17383e9a0ff6449765d78e88f2a001eb63cd80d
Jon Waters <waters1228@gmail.com>
2021-01-25 19:37:06 -0500

    Create controllers for each new report view

---
+++ src/Controller/ScheduleReportController.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ScheduleReportController implements Initializable {
+    
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
