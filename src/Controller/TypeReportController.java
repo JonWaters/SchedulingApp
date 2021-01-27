d122981080e18c56da5254f3f0ce1779a8faaa4b
Jon Waters <waters1228@gmail.com>
2021-01-26 20:04:51 -0500

    Add generateReeport() to TypeReportController

--- src/Controller/TypeReportController.java
+++ src/Controller/TypeReportController.java
@@ -1,5 +1,10 @@
 package Controller;
 
+import DAO.AppointmentDAO;
+import Model.Appointment;
+import Model.TypeReportDisplay;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,22 +15,28 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class TypeReportController implements Initializable {
 
-    @FXML
-    private TableView<?> reportTable;
+    private final DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
 
     @FXML
-    private TableColumn<?, ?> typeColumn;
+    private TableView<TypeReportDisplay> reportTable;
 
     @FXML
-    private TableColumn<?, ?> totalColumn;
+    private TableColumn<TypeReportDisplay, String> typeColumn;
+
+    @FXML
+    private TableColumn<TypeReportDisplay, Integer> totalColumn;
 
     @FXML
     private Label timestampLabel;
@@ -51,5 +62,51 @@ public class TypeReportController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        totalColumn.setCellValueFactory(new PropertyValueFactory<>("total"));
+
+        timestampLabel.setText(LocalDateTime.now().format(dateTimeFormat));
+
+        generateReport();
+    }
+
+    private void generateReport() {
+
+        try{
+            ObservableList<Appointment> dbAppointments = AppointmentDAO.selectAll();
+            ObservableList<String> appointmentTypes = FXCollections.observableArrayList();
+            ObservableList<TypeReportDisplay> typeReportList = FXCollections.observableArrayList();
+
+            //Get unique years
+            for (Appointment appointment : dbAppointments) {
+                String type = appointment.getType();
+
+                if (appointmentTypes.contains(type)) {
+                    continue;
+                } else {
+                    appointmentTypes.add(type);
+                }
+            }
+
+            String typeName;
+            int typeCount;
+
+            for (int i = 0; i < appointmentTypes.size(); i++) {
+                typeName = appointmentTypes.get(i);
+                typeCount = 0;
+                for (int k = 0; k < dbAppointments.size(); k++) {
+                    if (dbAppointments.get(k).getType().equals(appointmentTypes.get(i))) {
+                        typeCount++;
+                    }
+                }
+                TypeReportDisplay typeDisplay = new TypeReportDisplay(typeName, typeCount);
+                typeReportList.add(typeDisplay);
+            }
+
+            reportTable.setItems(typeReportList);
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
